378e7bd9395b761bdc80f616bce3e9b724c93f9a
Yoel Monzon <yoelfme@hotmail.com>
2015-05-28 23:56:04 -0600

    [update] routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,7 +8,13 @@
 Route::get('home', 'HomeController@index');
 
 Route::group(['namespace' => 'Admin', 'middleware' => []], function() {
-
+    Route::resource('books', 'BooksController');
+    Route::resource('prizes', 'PrizeController');
+    Route::resource('raffles', 'RaffleController');
+    Route::resource('seller', 'SellersController');
+    Route::resource('tickets', 'TicketsController');
+    Route::resource('users', 'UsersController');
+    Route::resource('winners', 'WinnersController');
 });
 
 Route::controllers([
